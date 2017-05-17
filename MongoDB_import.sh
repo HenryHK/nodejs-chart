4a70fcf7ce0c3542f9f92f30384c7cc83b01f6cf
lhan <henryhan.hku@gmail.com>
2017-05-18 00:03:30 +1000

    init thie repo

---
+++ MongoDB_import.sh
@@ -0,0 +1,3 @@
+ls -1 *.json | sed 's/.json$//' | while read col; do 
+    mongoimport -d db_name -c $col < $col.json; 
+done
---
+++ README.md
@@ -0,0 +1,3 @@
+# Node.js chart
+
+This is assignment 2 of COMP5347 Web Application Development, The University of Sydney.
